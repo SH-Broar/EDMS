a25ae9e855e3dcb30c482b68b880b331b0d39eaa
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-11-24 15:07:15 +0900

    opening image

--- Project/title_state.py
+++ Project/title_state.py
@@ -31,25 +31,8 @@ def handle_events():
     global startSelect, endSelect
     events = get_events()
     for event in events:
-        if(event.type == SDL_MOUSEMOTION):
-            mouseX = event.x
-            mouseY = 800 - event.y - 1
-            if mouseX > (1600 / 2) - (129 / 2) and  mouseX < (1600 / 2) + (129 / 2) and mouseY < 300 + (25 / 2) and mouseY > 300 - (25 / 2):
-                startSelect = 1
-            else:
-                startSelect = 0
-
-            if(mouseX > (1600 / 2) - (60 / 2) and mouseX < (1600 / 2) + (60 / 2) and mouseY < 250 + (20 / 2) and mouseY > 250 - (20 / 2)):
-                endSelect = 1
-            else:
-                endSelect = 0
-
-        elif(event.type == SDL_MOUSEBUTTONDOWN):
-            if startSelect == 1:
-                game_framework.change_state(main_state)
-            elif endSelect == 1:
-                game_framework.quit()
-
+        if event.type == SDL_QUIT:
+            game_framework.quit()
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
